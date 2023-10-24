4b8ebb8e5b67beb09df8493b76e4842ee9aeb959
Scott Baker <sbaker@glance.net>
2023-10-24 14:09:05 -0400

    added npm and gitignore

--- useBaapi.js
+++ useBaapi.js
@@ -2,5 +2,5 @@ console.log('script loaded');
 
 document.addEventListener("DOMContentLoaded", function() {
     // Your code that uses the external library goes here
-    console.log(GLANCE.Agent)
+    console.log(GLANCE)
   });
