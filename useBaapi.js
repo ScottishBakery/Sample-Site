fd31b9f7ef9347b86721a705abcc5d4c9969461a
Scott Baker <sbaker@glance.net>
2023-11-12 15:08:36 -0500

    auth thru ui

--- useBaapi.js
+++ useBaapi.js
@@ -1,6 +1,32 @@
-console.log('script loaded');
+document.addEventListener("DOMContentLoaded", function() {  
+  GLANCE.Agent.addEventListenerAll(function (eventname, eventdata) {
+    console.log({eventname, eventdata})
+  })
+  
+  const authenticate = (loginKey, partnerId, partnerUserId) => {
+    GLANCE.Agent.authenticate({
+      loginkey: loginKey,
+      partnerid: partnerId,
+      partneruserid: partnerUserId
+    })
+  }
 
-document.addEventListener("DOMContentLoaded", function() {
-    // Your code that uses the external library goes here
-    console.log(GLANCE)
-  });
+  const loginKeyInput = document.getElementById('loginKey')
+  const partnerIdInput = document.getElementById('partnerId')
+  const partnerUserIdInput = document.getElementById('partnerUserId')
+  const authButton = document.getElementById('authButton');
+
+  authButton.addEventListener('click', () => authenticate(loginKeyInput.value, partnerIdInput.value, partnerUserIdInput.value));
+
+  const keyInput = document.getElementById('sessionKey');
+  const joinButton = document.getElementById('joinButton');
+
+  const joinSession = (key) => {
+    console.log(key);
+    // console.log(`joining session with key ${key}`)
+    GLANCE.Agent.joinSession(key)
+  };
+
+  joinButton.addEventListener('click', () => joinSession(keyInput.value));
+});
+
