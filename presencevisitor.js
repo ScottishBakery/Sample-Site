86c109aca781ff462bad3f0961278424a1ca5c38
Scott Baker <sbaker@glance.net>
2024-02-08 08:44:57 +1100

    fixed visitor id

--- presencevisitor.js
+++ presencevisitor.js
@@ -1,7 +1,8 @@
 console.log('presence wrapper script loaded');
 
-// Instantiate a Visitor presence object
-var presencevisitor = new GLANCE.Presence.Visitor({
-    groupid : 21489, // optional, defaults to GLANCE_COBROWSE.groupid
-    visitorid : "jose" // optional, defaults to GLANCE_COBROWSE.visitorid
-  });
+document.addEventListener('DOMContentLoaded', () => {
+    const presencevisitor = new GLANCE.Presence.Visitor({
+        groupid : 21489, // optional, defaults to GLANCE_COBROWSE.groupid
+        visitorid : "jose" // optional, defaults to GLANCE_COBROWSE.visitorid
+    })
+})
