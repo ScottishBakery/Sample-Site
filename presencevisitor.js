f42a0c1ea053033b3c0172fedd8f40de658e1178
Scott Baker <sbaker@glance.net>
2024-02-08 08:47:30 +1100

    used code samples directly

--- presencevisitor.js
+++ presencevisitor.js
@@ -1,8 +1,19 @@
 console.log('presence wrapper script loaded');
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Instantiate a Visitor presence object
     const presencevisitor = new GLANCE.Presence.Visitor({
         groupid : 21489, // optional, defaults to GLANCE_COBROWSE.groupid
         visitorid : "jose" // optional, defaults to GLANCE_COBROWSE.visitorid
     })
+  
+    // Start sending regular presence updates (by default, every 60 seconds)
+    presencevisitor.presence();
+  
+    // Send a presence update (now) with some custom data
+    presencevisitor.presence({ data : { subdata : 3} })
+  
+    // Listen for agent messages.  
+    presencevisitor.onsignal = function (msg) { console.log("Received", msg); }
+    presencevisitor.connect();
 })
